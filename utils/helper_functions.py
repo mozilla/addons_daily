281d27f42d637c19784d7be18007d1e672dcbb9e
Brian Wright <bwright@bwright-4jh03y.local>
2019-03-04 14:04:52 -0800

    adding more scripts

--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -22,3 +22,37 @@ def histogram_mean(values):
     if denominator == 0:
         return None
     return numerator / float(denominator)
+
+
+def get_hist_avg(hist,just_keyed_hist):
+    """
+    :param hist: name of histogram of interest
+    :param just_keyed_hist: pyspark dataframe of
+    :return: returns a pyspark dataframe aggregated in the following form:
+    addon_id : mean(hist)
+    """
+    hist_data = (
+        just_keyed_hist
+        .filter(lambda x: hist in x.keys())
+        .map(lambda x: x[hist])
+        .flatMap(lambda x: [(i, histogram_mean(x[i]['values'])) for i in x.keys()])
+    )
+
+    agg_schema = StructType([StructField("addon_id", StringType(), True),
+                             StructField("avg_" + hist.lower(), FloatType(), True)])
+
+    return hist_data.toDF(schema=agg_schema)
+
+
+def dataframe_joiner(dfs):
+    """
+    Given a list of dataframes, join them all on "addon_id",
+    and return the joined dataframe
+    For use in keyed_histograms.py
+    :param dfs: list of pyspark aggregated dfs
+    :return: one joined df of all the dataframes in dfs
+    """
+    left = dfs[0]
+    for right in dfs[1:]:
+        left = left.join(right,on="addon_id",how=left)
+    return left
