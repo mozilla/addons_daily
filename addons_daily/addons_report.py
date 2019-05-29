1a2b59529b1b3eb8a87ef1a5ef4203b41b8e51f1
Sarah Melancon <sarahmelancon@sarahmelancon-txh3qf.local>
2019-05-29 13:46:09 -0700

    comment out events

--- addons_daily/addons_report.py
+++ addons_daily/addons_report.py
@@ -22,7 +22,7 @@ DEFAULT_TZ = "UTC"
 
 
 def agg_addons_report(
-    spark, main_summary_data, search_daily_data, events_data, raw_pings_data, **kwargs
+    spark, main_summary_data, search_daily_data, raw_pings_data, **kwargs
 ):
     """
     This function will create the addons dataset
@@ -72,7 +72,7 @@ def agg_addons_report(
     # search_daily = get_search_metrics(search_daily_data, addons_expanded)
 
     # install flow events metrics
-    install_flow_metrics = install_flow_events(events_data)
+    # install_flow_metrics = install_flow_events(events_data)
 
     # raw pings metrics
     page_load_times = get_page_load_times(spark, raw_pings_data)
@@ -93,7 +93,7 @@ def agg_addons_report(
         .join(top_ten_others, on="addon_id", how="left")
         .join(trend_metrics, on="addon_id", how="left")
         # .join(search_daily, on='addon_id', how='left')
-        .join(install_flow_metrics, on="addon_id", how="left")
+        # .join(install_flow_metrics, on="addon_id", how="left")
         .join(page_load_times, on="addon_id", how="left")
         .join(tab_switch_time, on="addon_id", how="left")
         .join(storage_get, on="addon_id", how="left")
@@ -141,22 +141,23 @@ def main(date,):
         .filter("sample_id < {}".format(sample))
     )
 
-    events = (
-        load_main_summary(
-            spark,
-            input_bucket="telemtry-parquet",
-            input_prefix="events",
-            input_version="v1",
-        )
-        .filter("submission_date_s3 == '{}'".format(date))
-        .filter("sample_id < {}".format(sample))
-    )
+    # events = (
+    #     load_main_summary(
+    #         spark,
+    #         input_bucket="telemtry-parquet",
+    #         input_prefix="events",
+    #         input_version="v1",
+    #     )
+    #     .filter("submission_date_s3 == '{}'".format(date))
+    #     .filter("sample_id < {}".format(sample))
+    # )
 
     raw_pings = load_raw_pings(sc)
 
     # bq_d = load_bq_data(datetime.date.today(), path, spark)
 
-    agg_data = agg_addons_report(spark, main_summary, search_daily, events, raw_pings)
+    # agg_data = agg_addons_report(spark, main_summary, search_daily, events, raw_pings)
+    agg_data = agg_addons_report(spark, main_summary, search_daily, raw_pings)
     print(agg_data.collect()[0:10])
     # return agg_data
 
