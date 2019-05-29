b2fd00e231bab488ce0f50f180d1775c4d860e8d
Brian Wright <bwright@bwright-4jh03y.local>
2019-05-29 16:04:09 -0700

    adding code

---
+++ code/addons_report.py
@@ -0,0 +1,167 @@
+import click
+import os
+from .utils.helpers import (
+    load_data_s3,
+    load_raw_pings,
+    get_spark,
+    get_sc,
+    load_keyed_hist
+)
+from .utils.telemetry_data import *
+from .utils.search_daily_data import *
+from .utils.events_data import *
+
+# from .utils.amo_data import *
+from .utils.bq_data import *
+from .utils.raw_pings import *
+from .utils.events_data import *
+from pyspark.sql import SparkSession
+
+DEFAULT_TZ = "UTC"
+
+
+def agg_addons_report(
+    spark,
+    #main_summary_data,
+    #search_daily_data,
+    raw_pings_data
+):
+    """
+    This function will create the addons dataset
+    """
+    # addons_and_users = main_summary_data.select(
+    #     "submission_date_s3",
+    #     "client_id",
+    #     F.explode("active_addons"),
+    #     "os",
+    #     "country",
+    #     "subsession_length",
+    #     "places_pages_count",
+    #     "places_bookmarks_count",
+    #     "scalar_parent_browser_engagement_total_uri_count",
+    #     "devtools_toolbox_opened_count",
+    #     "active_ticks",
+    #     "histogram_parent_tracking_protection_enabled",
+    #     "histogram_parent_webext_background_page_load_ms",
+    # )
+    #
+    # addons_expanded = addons_and_users.select(
+    #     "submission_date_s3",
+    #     "client_id",
+    #     "col.*",
+    #     "os",
+    #     "country",
+    #     "subsession_length",
+    #     "places_pages_count",
+    #     "places_bookmarks_count",
+    #     "scalar_parent_browser_engagement_total_uri_count",
+    #     "devtools_toolbox_opened_count",
+    #     "active_ticks",
+    #     "histogram_parent_tracking_protection_enabled",
+    #     "histogram_parent_webext_background_page_load_ms",
+    # ).cache()
+
+    keyed_histograms = load_keyed_hist(raw_pings_data)
+
+    # telemetry metrics
+    #user_demo_metrics = get_user_demo_metrics(addons_expanded)
+    #engagement_metrics = get_engagement_metrics(addons_expanded, main_summary)
+    #browser_metrics = get_browser_metrics(addons_expanded)
+    #top_ten_others = get_top_ten_others(addons_expanded)
+    #trend_metrics = get_trend_metrics(addons_expanded, main_summary_data)
+
+
+    # # search metrics
+    # search_metrics = get_search_metrics(search_daily_data, addons_expanded)
+
+    # install_flow_metrics = install_flow_events(events_data)
+    # raw pings metrics
+    page_load_times = get_page_load_times(spark, raw_pings_data)
+    tab_switch_time = get_tab_switch_time(spark, raw_pings_data)
+    storage_get = get_storage_local_get_time(keyed_histograms)
+    storage_set = get_storage_local_set_time(keyed_histograms)
+    startup_time = get_startup_time(keyed_histograms)
+    bkg_load_time = get_bkgd_load_time(keyed_histograms)
+    ba_popup_lt = get_ba_popup_load_time(keyed_histograms)
+    pa_popup_lt = get_pa_popup_load_time(keyed_histograms)
+    cs_injection_time = get_cs_injection_time(keyed_histograms)
+    mem_total = get_memory_total(keyed_histograms)
+
+    agg_data = (
+        os_dist.join(user_demo_metrics, on="addon_id", how="left")
+        .join(engagement_metrics, on="addon_id", how="left")
+        .join(browser_metrics, on="addon_id", how="left")
+        .join(top_ten_others, on="addon_id", how="left")
+        .join(trend_metrics, on="addon_id", how="left")
+        # .join(search_daily, on='addon_id', how='left')
+        # .join(install_flow_metrics, on="addon_id", how="left")
+        .join(page_load_times, on="addon_id", how="left")
+        .join(tab_switch_time, on="addon_id", how="left")
+        .join(storage_get, on="addon_id", how="left")
+        .join(storage_set, on="addon_id", how="left")
+        .join(startup_time, on="addon_id", how="left")
+        .join(bkg_load_time, on="addon_id", how="left")
+        .join(ba_popup_lt, on="addon_id", how="left")
+        .join(pa_popup_lt, on="addon_id", how="left")
+        .join(cs_injection_time, on="addon_id", how="left")
+        .join(mem_total, on="addon_id", how="left")
+        # .join(bq_data, on='addon_id', how='left')
+    )
+
+    return agg_data
+
+
+@click.command()
+@click.option("--date", required=True)
+@click.option("--sample", default=1, help="percent sample as int [1, 100]")
+def main(date, sample):
+    # path = '' # need to pass in from command line i think
+    # path var is a path to the user credentials.json for BQ
+    spark = get_spark(DEFAULT_TZ)
+    sc = get_sc()
+
+    # main_summary = (
+    #     load_main_summary(
+    #         spark,
+    #         input_bucket="telemetry-parquet",
+    #         input_prefix="main_summary",
+    #         input_version="v4",
+    #     )
+    #     .filter("submission_date_s3 == '{}'".format(date))
+    #     .filter("sample_id < {}".format(sample))
+    # )
+    #
+    # search_daily = (
+    #     load_main_summary(
+    #         spark,
+    #         input_bucket="telemetry-parquet",
+    #         input_prefix="search_clients_daily",
+    #         input_version="v4",
+    #     )
+    #     .filter("submission_date_s3 == '{}'".format(date))
+    #     .filter("sample_id < {}".format(sample))
+    # )
+    #
+    # events = (
+    #     load_main_summary(
+    #         spark,
+    #         input_bucket="telemetry-parquet",
+    #         input_prefix="events",
+    #         input_version="v1",
+    #     )
+    #     .filter("submission_date_s3 == '{}'".format(date))
+    #     .filter("sample_id < {}".format(sample))
+    # )
+
+    raw_pings = load_raw_pings(sc)
+
+    # bq_d = load_bq_data(datetime.date.today(), path, spark)
+
+    # agg_data = agg_addons_report(spark, main_summary, search_daily, events, raw_pings)
+    agg_data = agg_addons_report(spark, raw_pings)
+    print(agg_data.collect()[0:10])
+    # return agg_data
+
+
+if __name__ == "__main__":
+    main()
---
+++ code/utils/raw_pings.py
@@ -0,0 +1,248 @@
+from helpers import *
+import pyspark.sql.functions as F
+import pandas as pd
+from pyspark.sql import SQLContext
+
+
+def get_page_load_times(spark, df):
+    """
+    Function to aggreagte raw pings by addon_id and get average page load time
+    :param df: raw pings dataframe
+    :param spark: a spark instance
+    :return: aggregated dataframe
+    """
+    hist = "FX_PAGE_LOAD_MS_2"
+
+    avg_page_load = df.filter(lambda x: hist in x["payload"]["histograms"]).flatMap(
+        lambda x: [
+            (item, histogram_mean(x["payload"]["histograms"][hist]["values"]))
+            for item in x["environment"]["addons"]["activeAddons"].keys()
+        ]
+    )
+
+    schema = StructType(
+        [
+            StructField("addon_id", StringType(), True),
+            StructField("avg_page_loadtime", FloatType(), True),
+        ]
+    )
+
+    avg_page_load_df = spark.createDataFrame(data=avg_page_load, schema=schema)
+
+    avg_page_load_agg = (
+        avg_page_load_df.groupBy("addon_id")
+        .agg(F.mean("avg_page_loadtime"))
+        .withColumnRenamed("avg(avg_page_loadtime)", "avg_page_load_time")
+    )
+    return avg_page_load_agg
+
+
+def get_tab_switch_time(spark, df):
+    """
+    :param df: raw pings
+    :param spark: a spark instance
+    :return:
+    """
+    tab_switch_hist = (
+        df.filter(lambda x: "environment" in x)  #
+        .filter(lambda x: "addons" in x["environment"])  #
+        .filter(lambda x: "activeAddons" in x["environment"]["addons"])
+        .filter(lambda x: "payload" in x)  #
+        .filter(lambda x: "histograms" in x["payload"])  #
+        .filter(
+            lambda x: "FX_TAB_SWITCH_TOTAL_E10S_MS" in x["payload"]["histograms"]
+        )  #
+        .map(
+            lambda x: (
+                x["environment"]["addons"]["activeAddons"].keys(),
+                x["payload"]["histograms"]["FX_TAB_SWITCH_TOTAL_E10S_MS"],
+            )
+        )
+        .map(lambda x: [(i, x[1]) for i in x[0]])
+        .flatMap(lambda x: x)
+    )
+
+    tab_switch = tab_switch_hist.map(lambda x: (x[0], histogram_mean(x[1]["values"])))
+
+    tab_switch_df = (
+        spark.createDataFrame(tab_switch, ["addon_id", "tab_switch_ms"])
+        .groupby("addon_id")
+        .agg(F.mean("tab_switch_ms").alias("tab_switch_ms"))
+    )
+
+    return tab_switch_df
+
+
+##########################
+# storage local 'get' time
+##########################
+
+
+def get_storage_local_get_time(df):
+
+    storage_local_get_df = get_hist_avg("WEBEXT_STORAGE_LOCAL_GET_MS_BY_ADDONID", df)
+
+    storage_local_get_by_addon = storage_local_get_df.groupBy("addon_id").agg(
+        F.mean("avg_webext_storage_local_get_ms_by_addonid").alias(
+            "avg_storage_local_get_ms"
+        )
+    )
+    return storage_local_get_by_addon
+
+
+##########################
+# storage local 'set' time
+##########################
+
+
+def get_storage_local_set_time(df):
+    storage_local_set_df = get_hist_avg("WEBEXT_STORAGE_LOCAL_SET_MS_BY_ADDONID", df)
+
+    storage_local_set_by_addon = storage_local_set_df.groupBy("addon_id").agg(
+        F.mean("avg_webext_storage_local_set_ms_by_addonid").alias(
+            "avg_storage_local_set_ms"
+        )
+    )
+    return storage_local_set_by_addon
+
+
+##############
+# startup time
+##############
+
+
+def get_startup_time(df):
+    hist = "WEBEXT_EXTENSION_STARTUP_MS_BY_ADDONID"
+
+    ext_startup_df = get_hist_avg(hist, df)
+
+    startup_time_by_addon = ext_startup_df.groupBy("addon_id").agg(
+        F.mean("avg_WEBEXT_EXTENSION_STARTUP_MS_BY_ADDONID").alias("avg_startup_time")
+    )
+    return startup_time_by_addon
+
+
+###########################
+# background page load time
+###########################
+
+
+def get_bkgd_load_time(df):
+
+    hist = "WEBEXT_BACKGROUND_PAGE_LOAD_MS_BY_ADDONID"
+    return get_hist_avg(hist, df)
+
+
+#################################
+# browser action pop up load time
+#################################
+
+
+def get_ba_popup_load_time(df):
+
+    hist = "WEBEXT_BROWSERACTION_POPUP_OPEN_MS_BY_ADDONID"
+
+    ba_popup_load_time_df = get_hist_avg(hist, df)
+
+    ba_popup_load_time_by_addon = ba_popup_load_time_df.groupBy("addon_id").agg(
+        F.mean("avg_WEBEXT_BROWSERACTION_POPUP_OPEN_MS_BY_ADDONID").alias(
+            "avg_ba_popup_load_time"
+        )
+    )
+
+    return ba_popup_load_time_by_addon
+
+
+##############################
+# page action pop up load time
+##############################
+
+
+def get_pa_popup_load_time(df):
+    hist = "WEBEXT_PAGEACTION_POPUP_OPEN_MS_BY_ADDONID"
+
+    pa_popup_load_time_df = get_hist_avg(hist, df)
+
+    pa_popup_load_time_by_addon = pa_popup_load_time_df.groupBy("addon_id").agg(
+        F.mean("avg_WEBEXT_PAGEACTION_POPUP_OPEN_MS_BY_ADDONID").alias(
+            "avg_pa_popup_load_time"
+        )
+    )
+    return pa_popup_load_time_by_addon
+
+
+###############################
+# content script injection time
+###############################
+
+
+def get_cs_injection_time(df):
+    hist = "WEBEXT_CONTENT_SCRIPT_INJECTION_MS_BY_ADDONID"
+    content_script_time_df = get_hist_avg(hist, df)
+
+    content_script_time_by_addon = content_script_time_df.groupBy("addon_id").agg(
+        F.mean("avg_WEBEXT_CONTENT_SCRIPT_INJECTION_MS_BY_ADDONID").alias(
+            "avg_content_script_injection_ms"
+        )
+    )
+    return content_script_time_by_addon
+
+
+###############################
+# total memory usage
+###############################
+
+
+def get_memory_total(df):
+    hist = "MEMORY_TOTAL"
+    memory_total_df = get_hist_avg(hist, df)
+
+    memory_total_by_addon = memory_total_df.groupBy("addon_id").agg(
+        F.mean("avg_MEMORY_TOTAL").alias("avg_memory_total")
+    )
+    return memory_total_by_addon
+
+
+###############################
+# Performance Metrics - crashes
+###############################
+
+
+def get_crashes(spark, crash_pings):
+    """
+    :param crash_pings: raw crash pings dataframe
+    :return: aggregated crash by addon df
+    """
+    addon_time = (
+        crash_pings.filter(lambda x: "environment" in x)
+        .filter(lambda x: "addons" in x["environment"])
+        .filter(lambda x: "activeAddons" in x["environment"]["addons"])
+        .map(
+            lambda x: (
+                x["environment"]["addons"]["activeAddons"].keys(),
+                x["creationDate"],
+            )
+        )
+        .map(lambda x: [(i, x[1]) for i in x[0]])
+        .flatMap(lambda x: x)
+    )
+
+    dateToHour = F.udf(lambda x: x.hour, IntegerType())
+
+    addon_time_df = (
+        spark.createDataFrame(addon_time, ["addon_id", "time"])
+        .withColumn(
+            "time_stamp", F.to_timestamp("time", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+        )
+        .withColumn("hour", dateToHour("time_stamp"))
+        .drop("time", "time_stamp")
+    )
+
+    crashes_df = (
+        addon_time_df.groupby("addon_id", "hour")
+        .agg(F.count(F.lit(1)).alias("crashes"))
+        .groupby("addon_id")
+        .agg((F.sum("crashes") / F.sum("hour")).alias("avg_hourly_crashes"))
+    )
+
+    return crashes_df
