e14fe3ba01213e211d72af3564643c6f518f2e2d
benmiroglio <bmiroglio@mozilla.com>
2019-05-30 12:44:21 -0700

    Clear spark catalog cache

--- tests/test_telemetry.py
+++ tests/test_telemetry.py
@@ -15,122 +15,10 @@ def df_to_json(df):
     return [i.asDict() for i in df.collect()]
 
 
-# def load_expected_data(filename):
-#     root = os.path.dirname(__file__)
-#     path = os.path.join(root, "resources", filename)
-#     with open(path) as f:
-#         d = json.load(f)
-#     return d
-
-
 @pytest.fixture()
 def spark():
     spark_session = SparkSession.builder.appName("addons_daily_tests").getOrCreate()
     return spark_session
-
-
-# @pytest.fixture()
-# def addons_expanded(spark):
-#     d = load_expected_data("telemetry.json")
-#     addons_schema = StructType(
-#         [
-#             StructField("submission_date", StringType(), True),
-#             StructField("client_id", StringType(), True),
-#             StructField("addon_id", StringType(), True),
-#             StructField("blocklisted", BooleanType(), True),
-#             StructField("name", StringType(), True),
-#             StructField("user_disabled", BooleanType(), True),
-#             StructField("app_disabled", BooleanType(), True),
-#             StructField("version", StringType(), True),
-#             StructField("scope", IntegerType(), True),
-#             StructField("type", StringType(), True),
-#             StructField(
-#                 "scalar_parent_browser_engagement_tab_open_event_count",
-#                 IntegerType(),
-#                 True,
-#             ),
-#             StructField("foreign_install", BooleanType(), True),
-#             StructField("has_binary_components", BooleanType(), True),
-#             StructField("install_day", IntegerType(), True),
-#             StructField("update_day", IntegerType(), True),
-#             StructField("signed_state", IntegerType(), True),
-#             StructField("is_system", BooleanType(), True),
-#             StructField("is_web_extension", BooleanType(), True),
-#             StructField("multiprocess_compatible", BooleanType(), True),
-#             StructField("os", StringType(), True),
-#             StructField("country", StringType(), True),
-#             StructField("subsession_length", LongType(), True),
-#             StructField("places_pages_count", IntegerType(), True),
-#             StructField("places_bookmarks_count", IntegerType(), True),
-#             StructField(
-#                 "scalar_parent_browser_engagement_total_uri_count", IntegerType(), True
-#             ),
-#             StructField("devtools_toolbox_opened_count", IntegerType(), True),
-#             StructField("active_ticks", IntegerType(), True),
-#             StructField(
-#                 "histogram_parent_tracking_protection_enabled",
-#                 MapType(StringType(), IntegerType(), True),
-#                 True,
-#             ),
-#             StructField(
-#                 "histogram_parent_webext_background_page_load_ms",
-#                 MapType(StringType(), IntegerType(), True),
-#                 True,
-#             ),
-#         ]
-#     )
-#     return spark.createDataFrame(d, addons_schema)
-
-
-# @pytest.fixture()
-# def main_summary_uem(spark):
-#     schema = StructType(
-#         [
-#             StructField("disabled_addons_ids", ArrayType(StringType(), True), True),
-#             StructField("client_id", StringType(), True),
-#         ]
-#     )
-#     d = load_expected_data("uem.json")
-#     uem_df = spark.createDataFrame(d, schema)
-#     return uem_df
-
-
-# @pytest.fixture()
-# def main_summary_tto(spark):
-#     d = load_expected_data("mstto.json")
-#     schema = StructType(
-#         [
-#             StructField("client_id", StringType(), True),
-#             StructField(
-#                 "active_addons",
-#                 ArrayType(
-#                     StructType(
-#                         [
-#                             StructField("addon_id", StringType(), True),
-#                             StructField("blocklisted", BooleanType(), True),
-#                             StructField("name", StringType(), True),
-#                             StructField("user_disabled", BooleanType(), True),
-#                             StructField("app_disabled", BooleanType(), True),
-#                             StructField("version", StringType(), True),
-#                             StructField("scope", IntegerType(), True),
-#                             StructField("type", StringType(), True),
-#                             StructField("foreign_install", BooleanType(), True),
-#                             StructField("has_binary_components", BooleanType(), True),
-#                             StructField("install_day", IntegerType(), True),
-#                             StructField("update_day", IntegerType(), True),
-#                             StructField("signed_state", IntegerType(), True),
-#                             StructField("is_system", BooleanType(), True),
-#                             StructField("is_web_extension", BooleanType(), True),
-#                             StructField("multiprocess_compatible", BooleanType(), True),
-#                         ]
-#                     ),
-#                     True,
-#                 ),
-#                 True,
-#             ),
-#         ]
-#     )
-#     return spark.createDataFrame(d, schema)
 
 
 @pytest.fixture()
@@ -221,6 +109,7 @@ def test_browser_metrics(addons_expanded_day, spark):
             "pct_w_tracking_prot_enabled": 0.0,
         },
     ]
+    spark.catalog.clearCache()
     assert output == expected
 
 
@@ -258,8 +147,7 @@ def test_user_demo_metrics(addons_expanded_day, spark):
             "os_dist": {u"Windows_NT": 1.0},
         },
     ]
-    print("output", output)
-    print("expected", expected)
+    spark.catalog.clearCache()
     assert output == expected
 
 
@@ -299,7 +187,7 @@ def test_trend_metrics(addons_expanded, spark):
         {"addon_id": u"formautofill@mozilla.org", "dau": 1, "mau": 2, "wau": 1},
         {"addon_id": u"webcompat@mozilla.org", "dau": 1, "mau": 2, "wau": 1},
     ]
-    addons_expanded.unpersist()
+    spark.catalog.clearCache()
     assert output == expected_output
 
 
@@ -363,4 +251,5 @@ def test_engagement_metrics(addons_expanded_day, main_summary_day, spark):
             "disabled": None,
         },
     ]
+    spark.catalog.clearCache()
     assert output == expected_output
