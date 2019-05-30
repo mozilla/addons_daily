0ed1f54930b9a8d5bfc255599096b78534123cd6
benmiroglio <bmiroglio@mozilla.com>
2019-05-30 08:32:58 -0700

    Black check

--- tests/test_telemetry.py
+++ tests/test_telemetry.py
@@ -145,7 +145,7 @@ def test_browser_metrics(addons_expanded, spark):
     assert output == expected
 
 
-#@pytest.mark.skip(reason="skipping while sorting out py4j issue")
+# @pytest.mark.skip(reason="skipping while sorting out py4j issue")
 def test_user_demo_metrics(addons_expanded, spark):
     output = df_to_json(get_user_demo_metrics(addons_expanded))
     expected = load_expected_data("demo.json")
